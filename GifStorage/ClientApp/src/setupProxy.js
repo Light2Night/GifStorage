35f1ff2937041fcb829357c92a7b39ec24c3c02f
Приходько Владислав <3885vlad@gmail.com>
2023-11-15 15:01:14 +0200

    Added Swagger UI

--- GifStorage/ClientApp/src/setupProxy.js
+++ GifStorage/ClientApp/src/setupProxy.js
@@ -7,7 +7,8 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const context = [
   "/weatherforecast",
   "/api",
-  "/images"
+  "/images",
+  "swagger"
 ];
 
 const onError = (err, req, resp, target) => {
--- GifStorage/Program.cs
+++ GifStorage/Program.cs
@@ -10,10 +10,17 @@ builder.Services.AddControllersWithViews();
 var connectionString = builder.Configuration.GetConnectionString("SQLite");
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(connectionString));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment()) {
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+else {
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
